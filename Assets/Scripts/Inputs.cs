77ecd7c6d584d5b111e3ef7d00ff0f64a6a38a81
an <119092751+truongan240601@users.noreply.github.com>
2023-08-22 21:18:31 +0700

    Push

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Innovative.SolarCalculator;
+
+public class Inputs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
